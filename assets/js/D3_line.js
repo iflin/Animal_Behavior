39730c9be1a0f8527b78b69683e33b40c9dc0671
If LIn <if.lin87048@gmail.com>
2017-12-02 17:43:01 +0800

    完成歷屆投稿數量的D3互動折線圖。加入歷屆投稿數量資料。下次要微調文字重疊得狀況。

--- assets/js/D3_line.js
+++ assets/js/D3_line.js
@@ -1,11 +1,11 @@
 // Define margins, dimensions, and some line colors
 const margin = {top: 40, right: 120, bottom: 30, left: 40};
-const width = 800 - margin.left - margin.right;
+const width = 600 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Define the scales and tell D3 how to draw the line
-const x = d3.scaleLinear().domain([1910, 2010]).range([0, width]);     
-const y = d3.scaleLinear().domain([0, 40000000]).range([height, 0]);
+const x = d3.scaleLinear().domain([1993, 2017]).range([0, width]);     
+const y = d3.scaleLinear().domain([0, 350]).range([height, 0]);
 const line = d3.line().x(d => x(d.year)).y(d => y(d.population));
 
 const chart = d3.select('svg').append('g')
@@ -19,7 +19,7 @@ const xAxis = d3.axisBottom(x).tickFormat(d3.format('.4'));
 const yAxis = d3.axisLeft(y).tickFormat(d3.format('.2s'));
 chart.append('g').call(yAxis); 
 chart.append('g').attr('transform', 'translate(0,' + height + ')').call(xAxis);
-chart.append('text').html('State Population Over Time').attr('x', 200);
+chart.append('text').html('歷屆投稿文章數量').attr('id','gtitle').attr('x', 20).attr('y', -5);//圖標題
   
 // Load the data and draw a chart
 let states, tipBox;
@@ -37,14 +37,14 @@ d3.json('state-populations.json', d => {
   
   chart.selectAll()
     .data(states).enter()
-    .append('text')
+    .append('text') //曲線尾巴的文字
     .html(d => d.name)
     .attr('fill', d => d.color)
     .attr('alignment-baseline', 'middle')
     .attr('x', width)
     .attr('dx', '.5em')
-    .attr('y', d => y(d.currentPopulation)); 
-  
+    .attr('y', d => y(d.currentPopulation));
+    
   tipBox = chart.append('rect')
     .attr('width', width)
     .attr('height', height)
@@ -59,7 +59,12 @@ function removeTooltip() {
 }
 
 function drawTooltip() {
-  const year = Math.floor((x.invert(d3.mouse(tipBox.node())[0]) + 5) / 10) * 10;
+  
+//  設定滑鼠跟對齊線，跟Ｘ軸的相關關係_每10年
+//  const year = Math.floor((x.invert(d3.mouse(tipBox.node())[0]) + 5) / 10) * 10;
+    
+//  設定滑鼠跟對齊線，跟Ｘ軸的相關關係_每1年
+  const year = Math.floor(x.invert(d3.mouse(tipBox.node())[0])+1);
   
   states.sort((a, b) => {
     return b.history.find(h => h.year == year).population - a.history.find(h => h.year == year).population;
@@ -74,11 +79,13 @@ function drawTooltip() {
   
   tooltip.html(year)
     .style('display', 'block')
-    .style('left', (d3.event.pageX)+'px') //動態框位置
-    .style('top', (d3.event.pageY)+'px') //d3.event.pageY是滑鼠的位置函數
+//    .style('left', (d3.event.pageX-100)+'px') //資訊框位置跟著滑鼠跑
+//    .style('top', (d3.event.pageY-100)+'px') //d3.event.pageY是滑鼠的位置函數
+    .style('left', '100px') //固定資訊框位置
+    .style('top', '340px') //固定資訊框位置
     .selectAll()
     .data(states).enter()
     .append('div')
     .style('color', d => d.color)
-    .html(d => d.name + ': ' + d.history.find(h => h.year == year).population);
+    .html(d => d.name + ': ' + d.history.find(h => h.year == year).population + "篇");
 }
