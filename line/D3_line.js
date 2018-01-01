4c4cabb9564cac11df2a430dfeaa4c5ce825ba40
If LIn <if.lin87048@gmail.com>
2018-01-01 15:34:33 +0800

    1. 修改Line圖跟Bar圖的變數(margin, width, height, xy軸等等)，讓它們不衝突，現在都可以顯示在index中。(設計細節待修) 2. 增加各章節描述文字。(除了投稿題目外) 3. 變換底圖。 4. 未來預計：將網狀圖加入index、使D3圖們能RWD、調整D3圖的設計細節(顏色、字型等)、加入投稿題目搜尋功能、可以隨機換底圖、網狀圖可以改變匯入資料

--- line/D3_line.js
+++ line/D3_line.js
@@ -1,32 +1,35 @@
+
 // Define margins, dimensions, and some line colors
-var margin = {top: 40, right: 120, bottom: 30, left: 40};
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+var margin_line = {top: 40, right: 120, bottom: 30, left: 40};
+var width_line = 600 - margin_line.left - margin_line.right;
+var height_line = 400 - margin_line.top - margin_line.bottom;
+
 
 // Define the scales and tell D3 how to draw the line
-var x = d3.scaleLinear().domain([1992, 2017]).range([0, width]);     
-var y = d3.scaleLinear().domain([0, 350]).range([height, 0]);
-var line = d3.line().x(d => x(d.year)).y(d => y(d.population));
+var x_line = d3.scaleLinear().domain([1992, 2017]).range([0, width_line]);     
+var y_line = d3.scaleLinear().domain([0, 350]).range([height_line, 0]);
+var line = d3.line().x(d => x_line(d.year)).y(d => y_line(d.population));
 
-var chart = d3.select('#paper_line').append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var line_chart = d3.select('#paper_line').append('g')
+  .attr('transform', 'translate(' + margin_line.left + ',' + margin_line.top + ')');
   
-var tooltip = d3.select('#line_tooltip');
-var tooltipLine = chart.append('line');
+//var tooltip = d3.select('#line_tooltip');
+var line_tooltip = d3.select("#papers").append("div").attr("id", "line_tooltip");
+var tooltipLine = line_chart.append('line');
   
 // Add the axes and a title
-var xAxis = d3.axisBottom(x).tickFormat(d3.format('.4'));
-var yAxis = d3.axisLeft(y).tickFormat(d3.format('.2s'));
-chart.append('g').call(yAxis); 
-chart.append('g').attr('transform', 'translate(0,' + height + ')').call(xAxis);
-chart.append('text').html('歷屆投稿文章數量').attr('id','gtitle').attr('x', 20).attr('y', -5);//圖標題
+var xAxis_line = d3.axisBottom(x_line).tickFormat(d3.format('.4'));
+var yAxis_line = d3.axisLeft(y_line).tickFormat(d3.format('.2s'));
+line_chart.append('g').call(yAxis_line); 
+line_chart.append('g').attr('transform', 'translate(0,' + height_line + ')').call(xAxis_line);
+line_chart.append('text').html('歷屆投稿文章數量').attr('id','gtitle').attr('x', 20).attr('y', -5);//圖標題
   
 // Load the data and draw a chart
 let states, tipBox;
-d3.json('data/year-papers.json', d => {
+d3.json('line/year-papers.json', d => {
   states = d;
 
-  chart.selectAll()
+  line_chart.selectAll()
     .data(states).enter()
     .append('path')
     .attr('fill', 'none')
@@ -35,26 +38,26 @@ d3.json('data/year-papers.json', d => {
     .datum(d => d.history)
     .attr('d', line);
   
-  chart.selectAll()
+  line_chart.selectAll()
     .data(states).enter()
     .append('text') //曲線尾巴的文字
     .html(d => d.name)
     .attr('fill', d => d.color)
     .attr('alignment-baseline', 'middle')
-    .attr('x', width-5)
+    .attr('x', width_line-5)
     .attr('dx', '.5em')
-    .attr('y', d => y(d.tagY));//曲線尾巴的文字的Y軸位置
+    .attr('y', d => y_line(d.tagY));//曲線尾巴的文字的Y軸位置
     
-  tipBox = chart.append('rect')
-    .attr('width', width)
-    .attr('height', height)
+  tipBox = line_chart.append('rect')
+    .attr('width', width_line)
+    .attr('height', height_line)
     .attr('opacity', 0)
     .on('mousemove', drawTooltip)
     .on('mouseout', removeTooltip);
 })
 
 function removeTooltip() {
-  if (tooltip) tooltip.style('display', 'none');
+  if (line_tooltip) line_tooltip.style('display', 'none');
   if (tooltipLine) tooltipLine.attr('stroke', 'none');
 }
 
@@ -64,7 +67,7 @@ function drawTooltip() {
 //  var year = Math.floor((x.invert(d3.mouse(tipBox.node())[0]) + 5) / 10) * 10;
     
 //  設定滑鼠跟對齊線，跟Ｘ軸的相關關係_每1年
-  var year = Math.floor(x.invert(d3.mouse(tipBox.node())[0])+1);
+  var year = Math.floor(x_line.invert(d3.mouse(tipBox.node())[0])+1);
   
   states.sort((a, b) => {
     return b.history.find(h => h.year == year).population - a.history.find(h => h.year == year).population;
@@ -72,12 +75,12 @@ function drawTooltip() {
   
   //對齊線的動態位置    
   tooltipLine.attr('stroke', 'black')
-    .attr('x1', x(year))
-    .attr('x2', x(year))
+    .attr('x1', x_line(year))
+    .attr('x2', x_line(year))
     .attr('y1', 0)
-    .attr('y2', height);
+    .attr('y2', height_line);
   
-  tooltip.html(year)
+ line_tooltip.html(year)
     .style('display', 'block')
 //    .style('left', (d3.event.pageX-100)+'px') //資訊框位置跟著滑鼠跑
 //    .style('top', (d3.event.pageY-100)+'px') //d3.event.pageY是滑鼠的位置函數
